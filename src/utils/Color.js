5afb0b09bf9387823bceca4867dfe96e2ebf5c29
Supremoestelar369 <111463579+Supremoestelar369@users.noreply.github.com>
2022-09-19 18:18:42 -0500

    primer commit,creacion proyecto

---
+++ src/utils/Color.js
@@ -0,0 +1,4 @@
+const color = ["#196e8b", "#dbe6e1", "#24a8ea55", "#F4D03F", "#7FB3D5", "#F8C471",
+"#ABB2B9", "#A9CCE3", "#E74C3C","#F1C40F", "#EB984E","#229954", "#D2B4DE"]
+
+export default color 
